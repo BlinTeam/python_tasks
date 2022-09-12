4fb72247145f5b839ca70d3e34c080a5b794ed8a
cruelscript <cruelscript@gmail.com>
2022-09-12 15:56:27 +0300

    Moved files to other repo

--- algorithms/sorts/insertion_sort.py
+++
@@ -1,8 +0,0 @@
-def insertion_sort(elements):
-    for i in range(1, len(elements)):
-        anchor = elements[i]
-        j = i - 1
-        while j >= 0 and anchor < elements[j]:
-            elements[j + 1] = elements[j]
-            j = j - 1
-        elements[j + 1] = anchor
